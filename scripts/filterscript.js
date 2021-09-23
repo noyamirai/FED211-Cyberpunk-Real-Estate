6aa0f455ad7432b349aec2c591215ada09ecbecb
Maijla Ikiz <planet1109@hotmail.com>
2021-09-23 18:09:37 +0200

    fixed: mobile menu

--- scripts/filterscript.js
+++ scripts/filterscript.js
@@ -2,10 +2,12 @@ console.log("FILTER SCRIPT ACTIVE");
 
 const filterMenuBtn = document.querySelector("#filter-menu-btn");
 const filterMenu = document.querySelector(".filters");
+const cardSection = document.querySelector(".card-section");
 
 filterMenuBtn.addEventListener("click", () => {
     console.log('filter menu icon clicked');
 
     filterMenu.classList.toggle("open");
     filterMenuBtn.classList.toggle("open");
+    // cardSection.classList.toggle("hide");
 })
