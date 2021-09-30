8c74a1aeaaee85ed82c7a14b18f531d7630333cc
Maijla Ikiz <planet1109@hotmail.com>
2021-09-30 13:52:10 +0200

    added: styling for result page including dynamic li elements

--- scripts/togglescript.js
+++ scripts/togglescript.js
@@ -25,3 +25,28 @@ toggleRentBtn.addEventListener("click", function() {
   }
 
 });
+
+// BRON: https://www.w3schools.com/howto/howto_js_trigger_button_enter.asp
+// Execute a function when the user releases a key on the keyboard
+toggleContainer.addEventListener("keyup", function(event) {
+  // Number 13 is the "Enter" key on the keyboard
+  if (event.keyCode === 13) {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    toggleContainer.classList.toggle("toggled");
+
+    if (!toggleRentBtn.classList.contains("toggle__option--active")) {
+      toggleSaleBtn.classList.toggle("toggle__option--active");
+      toggleRentBtn.classList.toggle("toggle__option--active");
+    } else if(!toggleSaleBtn.classList.contains("toggle__option--active")) {
+      toggleSaleBtn.classList.toggle("toggle__option--active");
+      toggleRentBtn.classList.toggle("toggle__option--active");
+
+    }
+  }
+});
+
+// BRON: https://stackoverflow.com/questions/6833914/how-to-prevent-the-confirm-form-resubmission-dialog
+if ( window.history.replaceState ) {
+  window.history.replaceState( null, null, window.location.href );
+}
